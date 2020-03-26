9b4fa5c0fdcdf1480682c5c81029b2bac5783824
Oydrey <oydrey@gmail.com>
2020-03-26 16:58:58 +0100

    version qui fonctionne

--- app/src/main/java/com/oydreyproduction/partymanager/MainActivity.java
+++ app/src/main/java/com/oydreyproduction/partymanager/MainActivity.java
@@ -3,7 +3,6 @@ package com.oydreyproduction.partymanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,8 +15,6 @@ public class MainActivity extends AppCompatActivity {
     Button button18;
     Button button19;
 
-    DAOBase daoBase;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,16 +25,6 @@ public class MainActivity extends AppCompatActivity {
         button17 = findViewById(R.id.button17);
         button18 = findViewById(R.id.button18);
         button19 = findViewById(R.id.button19);
-
-        daoBase = new DAOBase(this) {
-            @Override
-            public SQLiteDatabase open() {
-                return super.open();
-            }
-        };
-
-        daoBase.close();
-
     }
 
     @Override
